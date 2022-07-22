b891c3070e9fea4bb949df631384760a1dd4bb4e
UncleDave <3244206+UncleDave@users.noreply.github.com>
2022-07-22 23:41:29 +0200

    stfu eslint

--- scripts/copy-sounds-to-blob-storage.ts
+++ scripts/copy-sounds-to-blob-storage.ts
@@ -7,6 +7,7 @@ const soundsDirectory = 'projects/bot/sounds';
 
 (async function run() {
   const files = await fs.readdir(soundsDirectory);
+  // eslint-disable-next-line max-len
   const filesService = new FilesService('DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://localhost:10000/devstoreaccount1;');
 
   await Promise.all(files.map(fileName => filesService.saveFile(fileName, createReadStream(path.join(soundsDirectory, fileName)))));
